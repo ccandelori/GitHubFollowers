596e697366eda599c97edafa887078d72bdcda04
ccandelori <cameron.nigro@gmail.com>
2023-11-05 17:29:31 -0500

    Initialize repo

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Cameron Candelori on 10/30/23.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Cameron Candelori on 10/30/23.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(backgroundColor: UIColor, title: String)  {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        titleLabel?.textColor = .white
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
