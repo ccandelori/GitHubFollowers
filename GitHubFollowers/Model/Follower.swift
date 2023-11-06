e8e45232832d35e65ddaec9677beff14e28624ed
ccandelori <cameron.nigro@gmail.com>
2023-11-06 15:16:50 -0500

    Add User and Follower models to project because they somehow got created in another directory because I'm a big dummy.

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Cameron Candelori on 11/6/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
